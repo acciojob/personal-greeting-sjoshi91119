4ad62452211b93ee23f482b22a1ac3304fd2feb9
Shubham Joshi <sjoshi91119@gmail.com>
2023-06-28 19:29:43 +0530

    submitting code-Wed Jun 28 2023 19:29:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <input type="text"></input>
 
-export default App
+      <p>Hello {input.value}!</p>
+    </div>
+  );
+};
+
+export default App;
